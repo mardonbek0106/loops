24c338bad2be428e5aff67bfdfccee75b28f3491
Mardonbek <mardonbek.00.tursunaliyev@gmail.com>
2023-08-27 11:53:23 +0500

    for loop darsi

---
+++ app.js
@@ -0,0 +1,118 @@
+// SWITCH CASE (haqida)
+
+// let data = "yomgir";
+// switch (data){
+//     case "yomgir":
+//         console.log("bugu yomgir");break
+//     case "qor":
+//         console.log("bugun sovuq");
+// }
+
+
+// FOR LOOP (haqida)
+
+// let i = 1
+// for (i; i < 10; i ++){
+//     console.log("salom nihon");
+   
+// }
+// let i = 1;
+// for (i; i <= 10;){
+// 	console.log("salom yaponya");
+// }
+
+
+
+// let i = 1;
+// while(i <= 100){
+// 	if(i % 2 === 1)console.log(i);
+// 	i++;
+// }
+// console.log("shu yerda code tugadi");
+
+//    let i = 1;
+//    let sum = 0;
+//    while(i <= 100){
+// 	if(i % 2 === 0) {
+// 		sum += i
+// 		console.log(i,"son");
+// 		console.log(sum, "yegindi");
+// 	}
+// 	i++;
+//    }
+// let i = 1;
+// let sum = 0;
+// while(i <= 200){
+// 	if(i % 2 === 1)sum += i;
+// 	i++;
+// }
+// console.log(sum);				
+
+// let i = 1;
+// let sum = 0;
+// while(i <= 100){
+// 	if(i % 2 === 1){
+// 		sum +=i;
+// 	console.log(i, "son");
+// 	console.log(sum, "yegindi");
+// 	}
+// 	i++;
+// }
+// console.log(sum);
+
+// let i = 1;
+// let sum = 0;
+// while(i <= 50){
+// if(i % 2 === 0){
+// 	sum += i;
+// 	console.log(i, "son");
+// 	console.log(sum, "yegindi");
+// }
+// 	i++;
+// }
+// console.log(sum);
+
+// let i = 1;
+// while(i <= 100){
+// 	if(i % 2 === 1)
+// 	console.log(i, "juft son");
+// i++;
+// }
+// let i = 1;
+// while(i <= 100){
+// 	console.log(i);
+// 	i++;
+// }
+
+// let i = 1;
+// while(i <= 100){
+// 	if(i % 2 === 1)
+// 	console.log(i, "toq son");
+// 	i++;
+// }
+
+// let i = 1;
+// let sum = 0;
+// while(i <= 50){
+// 	if(i % 2 === 0){
+// 	sum += i;
+// console.log(i,"son");
+// console.log(sum,"yegindi");
+// }
+// i++;
+// }
+// do while 
+// let i = 0;
+// do{
+// 	console.log(i);
+// 	i++
+// }while(i <= 100);
+// console.log(i, "ohirgi qiymati");
+
+// let n = 5;
+// let factorial = 1;
+// do{
+// 	factorial *= n;
+// 	n--;
+// }while(n > 0)
+// console.log(factorial);
